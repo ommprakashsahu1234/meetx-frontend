885a6dc165a41eb0faffd45a25f056b74965ad07
Omm Prakash Sahu <opsommprakash@gmail.com>
2025-08-10 17:16:43 +0530

    first commit

--- src/components/utils/axios.js
+++ src/components/utils/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000/api/",
-  // baseURL: "https://meexapp-backend.onrender.com/api/",
+  // baseURL: "http://localhost:5000/api/",
+  baseURL: "https://meetx-backend-a0kt.onrender.com/api/",
 });
 
 instance.interceptors.request.use((config) => {
